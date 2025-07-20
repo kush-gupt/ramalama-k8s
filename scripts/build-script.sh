7f2ef1abefa6ecde4f7b03b60dacfe565af0dc7a
Kush Gupta <kushalgupta@gmail.com>
2025-07-20 16:02:38 -0400

    run on main llama.cpp
    Signed-off-by: Kush Gupta <kushalgupta@gmail.com>

--- scripts/build-script.sh
+++ scripts/build-script.sh
@@ -163,13 +163,11 @@ configure_common_flags() {
 }
 
 clone_and_build_llama_cpp() {
-  local llama_cpp_sha="97340b4c9924be86704dbf155e97c8319849ee19"
   local install_prefix
   install_prefix=$(set_install_prefix)
   git clone https://github.com/ggml-org/llama.cpp
   cd llama.cpp
   git submodule update --init --recursive
-  git reset --hard "$llama_cpp_sha"
   cmake_steps "${common_flags[@]}"
   install -m 755 build/bin/rpc-server "$install_prefix"/bin/rpc-server
   cd ..
